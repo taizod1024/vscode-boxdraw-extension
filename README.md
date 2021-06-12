f19b481dbeaff3a540e82009c052827366baa303
yamamoto.taizo <taizod1024@gmail.com>
2021-06-13 06:50:20 +0900

    wip: 定期コミット

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Status-WIP](https://img.shields.io/badge/Status-WIP-orange)
 
-Boxdraw extension make it easy to draw simple ASCII diagrams.
+Boxdraw extension make it easy to draw simple text diagrams.
 
 ## features
 
@@ -13,4 +13,8 @@ Boxdraw extension make it easy to draw simple ASCII diagrams.
 ## usage
 
 wip
+
+## thanks
+
+The basic idea is inspired by xyzzy boxdraw.l .
 
--- src/BoxdrawExtension.ts
+++ src/BoxdrawExtension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+var eaw = require('eastasianwidth');
 
 // extension core
 class BoxdrawExtension {
@@ -10,6 +11,7 @@ class BoxdrawExtension {
     public appname: string;
     public appid: string;
     public applabel: string;
+    public boxchars: { char: string, val: number }[];
 
     // context
     public mode: boolean;
@@ -26,6 +28,58 @@ class BoxdrawExtension {
         this.appname = "boxdraw";
         this.appid = "boxdraw-extension";
         this.applabel = "BOXDRAW";
+        // the basic idea is inspired by xyzzy boxdraw.l
+        //    4        64
+        //    |        |
+        // 1 -+- 2  16-+-32
+        //    |        |
+        //    8        128
+        this.boxchars = [
+            { char: "─", val: 0b00000011 },
+            { char: "│", val: 0b00001100 },
+            { char: "┌", val: 0b00001010 },
+            { char: "┐", val: 0b00001001 },
+            { char: "┘", val: 0b00000101 },
+            { char: "└", val: 0b00000110 },
+            { char: "├", val: 0b00001110 },
+            { char: "┬", val: 0b00001011 },
+            { char: "┤", val: 0b00001101 },
+            { char: "┴", val: 0b00000111 },
+            { char: "┼", val: 0b00001111 },
+            { char: "━", val: 0b00110000 },
+            { char: "┃", val: 0b11000000 },
+            { char: "┏", val: 0b10100000 },
+            { char: "┓", val: 0b10010000 },
+            { char: "┛", val: 0b01010000 },
+            { char: "┗", val: 0b01100000 },
+            { char: "┣", val: 0b11100000 },
+            { char: "┳", val: 0b10110000 },
+            { char: "┫", val: 0b11010000 },
+            { char: "┻", val: 0b01110000 },
+            { char: "╋", val: 0b11110000 },
+            { char: "┠", val: 0b11000010 },
+            { char: "┯", val: 0b00111000 },
+            { char: "┨", val: 0b11000001 },
+            { char: "┷", val: 0b00110100 },
+            { char: "┿", val: 0b00111100 },
+            { char: "┝", val: 0b00101100 },
+            { char: "┰", val: 0b10000011 },
+            { char: "┥", val: 0b00011100 },
+            { char: "┸", val: 0b01000011 },
+            { char: "╂", val: 0b11000011 },
+            { char: "─", val: 0b00000001 },
+            { char: "─", val: 0b00000010 },
+            { char: "│", val: 0b00000100 },
+            { char: "│", val: 0b00001000 },
+            { char: "━", val: 0b00010000 },
+            { char: "━", val: 0b00100000 },
+            { char: "┃", val: 0b01000000 },
+            { char: "┃", val: 0b10000000 },
+            { char: "→", val: 0b00000011 },
+            { char: "←", val: 0b00000011 },
+            { char: "↑", val: 0b00001100 },
+            { char: "↓", val: 0b00001100 },
+        ];
     }
 
     public activate(context: vscode.ExtensionContext) {
@@ -43,73 +97,79 @@ class BoxdrawExtension {
         // command
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.toggleMode`, () => { boxdrawextension.toggleMode(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.toggleBold`, () => { boxdrawextension.toggleBold(); }));
+
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawLeft`, () => { boxdrawextension.drawLeft(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawRight`, () => { boxdrawextension.drawRight(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawUp`, () => { boxdrawextension.drawUp(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawDown`, () => { boxdrawextension.drawDown(); }));
+
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawLeftArrow`, () => { boxdrawextension.drawLeftArrow(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawRightArrow`, () => { boxdrawextension.drawRightArrow(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawUpArrow`, () => { boxdrawextension.drawUpArrow(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.drawDownArrow`, () => { boxdrawextension.drawDownArrow(); }));
+
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.clearLeft`, () => { boxdrawextension.clearLeft(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.clearRight`, () => { boxdrawextension.clearRight(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.clearUp`, () => { boxdrawextension.clearUp(); }));
         context.subscriptions.push(vscode.commands.registerCommand(`${this.appid}.clearDown`, () => { boxdrawextension.clearDown(); }));
+
         // statusbar
         this.statusbaritem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
         this.statusbaritem.command = `${this.appid}.toggleMode`;
         this.statusbaritem.show();
         context.subscriptions.push(this.statusbaritem);
+
         // setcontext
         this.setMode(false, true);
         this.setBold(false, true);
     }
 
     // public interface
-    public toggleMode() {
-        this.setMode(!this.mode);
-    }
-    public toggleBold() {
-        this.setBold(!this.bold);
-    }
-    public drawLeft() {
-        this.channel.appendLine(`[${this.timestamp()}] drawLeft`);
-    }
-    public drawRight() {
-        this.channel.appendLine(`[${this.timestamp()}] drawRight`);
-    }
-    public drawUp() {
-        this.channel.appendLine(`[${this.timestamp()}] drawUp`);
-    }
-    public drawDown() {
-        this.channel.appendLine(`[${this.timestamp()}] drawDown`);
-    }
-    public drawLeftArrow() {
-        this.channel.appendLine(`[${this.timestamp()}] drawLeftArrow`);
-    }
-    public drawRightArrow() {
-        this.channel.appendLine(`[${this.timestamp()}] drawRightArrow`);
-    }
-    public drawUpArrow() {
-        this.channel.appendLine(`[${this.timestamp()}] drawUpArrow`);
-    }
-    public drawDownArrow() {
-        this.channel.appendLine(`[${this.timestamp()}] drawDownArrow`);
-    }
-    public clearLeft() {
-        this.channel.appendLine(`[${this.timestamp()}] clearLeft`);
-    }
-    public clearRight() {
-        this.channel.appendLine(`[${this.timestamp()}] clearRight`);
-    }
-    public clearUp() {
-        this.channel.appendLine(`[${this.timestamp()}] clearUp`);
-    }
-    public clearDown() {
-        this.channel.appendLine(`[${this.timestamp()}] clearDown`);
+    public toggleMode() { this.setMode(!this.mode); }
+    public toggleBold() { this.setBold(!this.bold); }
+
+    public drawLeft() { this.drawBox(0b00000001, false, false); }
+    public drawRight() { this.drawBox(0b00000010, false, false); }
+    public drawUp() { this.drawBox(0b00000100, false, false); }
+    public drawDown() { this.drawBox(0b00001000, false, false); }
+
+    public drawLeftArrow() { this.drawBox(0b00000001, true, false); }
+    public drawRightArrow() { this.drawBox(0b00000010, true, false); }
+    public drawUpArrow() { this.drawBox(0b00000100, true, false); }
+    public drawDownArrow() { this.drawBox(0b00001000, true, false); }
+
+    public clearLeft() { this.drawBox(0b00000001, false, true); }
+    public clearRight() { this.drawBox(0b00000010, false, true); }
+    public clearUp() { this.drawBox(0b00000100, false, true); }
+    public clearDown() { this.drawBox(0b00001000, false, true); }
+
+    // inner interface
+    protected drawBox(direction: number, isarrow: boolean, isclear: boolean) {
+        this.channel.appendLine(`[${this.timestamp()}] direction=${direction}, isarrow=${isarrow}, isclear=${isclear}`);
+        // check editor
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+        // check text
+        const document = editor.document;
+        const selection = editor.selection;
+        const curpos = selection.active;
+        // edit
+        editor.edit((builder: vscode.TextEditorEdit) => {
+            //    builder.insert(curpos, "abc");
+            // WIP 処理
+            var startpos = new vscode.Position(curpos.line, 0);
+            var endpos = new vscode.Position(curpos.line, curpos.character);
+            var range = new vscode.Range(startpos, endpos);
+            var text = document.getText(range);
+            text = text + ":" + eaw.length(text);
+            this.channel.appendLine(text);
+            // 
+        });
     }
 
-    // model
+    // oops box
+
+    // ops vscoide
     protected setMode(mode: boolean, force = false) {
         if (this.mode != mode || force) {
             this.mode = mode;
@@ -127,9 +187,10 @@ class BoxdrawExtension {
         }
     }
     protected updateStatusbar() {
-        let msg = this.applabel;
-        if (this.bold) msg += "(bold)";
-        if (this.mode) msg += "$(edit)";
+        let msg = "";
+        msg += this.mode ? "$(edit)" : "$(stop-circle)"
+        msg += this.applabel;
+        msg += this.bold ? "(bold)" : "";
         this.statusbaritem.text = msg;
     }
 
